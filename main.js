832c2df31bcee363f50ae057b8124cb1ffaab336
DasNilima <nilima_sust@yahoo.com>
2024-02-05 20:35:44 -0800

    add accessibility features

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './scss/style.scss';
 
 const body = document.querySelector('body');
+const main = document.querySelector('main');
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
 const media = window.matchMedia('(width < 69.375em)');
@@ -11,18 +12,25 @@ function openMobileMenu() {
     console.log('open menu');
     btnOpen.setAttribute('aria-expanded', 'true');
     body.classList.add('noscroll');
+    navContent.removeAttribute('inert');
+    main.setAttribute('inert', '');
+    btnClose.focus();
 }
 
 function closeMobileMenu() {
     console.log('close menu');
     btnOpen.setAttribute('aria-expanded', 'false');
     body.classList.remove('noscroll');
+    navContent.setAttribute('inert', '');
+    main.removeAttribute('inert');
+    btnOpen.focus();
 }
 
 function setupNav(e) {
     if (e.matches) {
         // is mobile
         console.log('is mobile');
+        navContent.setAttribute('inert', '');
         
         setTimeout(() => {
             navContent.style.display = 'block';
@@ -32,6 +40,8 @@ function setupNav(e) {
     else {
         // is desktop
         console.log('is desktop');
+        navContent.removeAttribute('inert');
+        main.removeAttribute('inert');
         navContent.style.display = '';
     }
 }
@@ -43,4 +53,10 @@ btnClose.addEventListener('click', closeMobileMenu);
 media.addEventListener('change', function (e) {
     // console.log('window.matchMedia change = ' + e.matches);
     setupNav(e);
+});
+
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'Tab') {
+        console.log(document.activeElement);
+    }
 });
